123328761b6f8cbb137d64b0ac4685652a2c91e1
wdxujyf <wdxyjyf@qq.com>
2018-03-11 12:03:17 +0800

    后台最新版本
    后台最新版本

--- your-project-name/resources/views/admin/index.blade.php
+++ your-project-name/resources/views/admin/index.blade.php
@@ -37,9 +37,8 @@
                 </a>
                 <ul class="sub-menu">
                     <li>
-<<<<<<< HEAD
-                        <a _href="admin-list.html">
-=======
+
+
                         <a _href="{{url('admin/homeindex')}}">
                             <i class="iconfont">&#xe6a7;</i>
                             <cite>会员列表</cite>
@@ -48,23 +47,7 @@
                     </li >
                 </ul>
             </li>
-            <li>
-                <a href="javascript:;">
-                    <i class="iconfont">&#xe723;</i>
-                    <cite>订单管理</cite>
-                    <i class="iconfont nav_right">&#xe697;</i>
-                </a>
-                <ul class="sub-menu">
-                    <li>
-                        <a _href="order-list.html">
->>>>>>> f28309157611839e0488bbbea5737a9b5aa7a4b6
-                            <i class="iconfont">&#xe6a7;</i>
-                            <cite>会员列表</cite>
-                        </a>
-                    </li >
-                </ul>
-                
-            </li>
+            
             <li>
                 <a href="javascript:;">
                     <i class="iconfont">&#xe6b8;</i>
@@ -133,71 +116,7 @@
                   
                 </ul>
             </li>
-<<<<<<< HEAD
-           
-=======
-<<<<<<< HEAD
-            <li>
-                <a href="javascript:;">
-                    <i class="iconfont">&#xe6ce;</i>
-                    <cite>系统统计</cite>
-                    <i class="iconfont nav_right">&#xe697;</i>
-                </a>
-                <ul class="sub-menu">
-                    <li>
-                        <a _href="{{url('banner')}}">
-                            <i class="iconfont">&#xe6a7;</i>
-                            <cite>轮播管理</cite>
-                        </a>
-                    </li >
-                    <li>
-                        <a _href="echarts2.html">
-                            <i class="iconfont">&#xe6a7;</i>
-                            <cite>柱状图</cite>
-                        </a>
-                    </li>
-                    <li>
-                        <a _href="echarts3.html">
-                            <i class="iconfont">&#xe6a7;</i>
-                            <cite>地图</cite>
-                        </a>
-                    </li>
-                    <li>
-                        <a _href="echarts4.html">
-                            <i class="iconfont">&#xe6a7;</i>
-                            <cite>饼图</cite>
-                        </a>
-                    </li>
-                    <li>
-                        <a _href="echarts5.html">
-                            <i class="iconfont">&#xe6a7;</i>
-                            <cite>雷达图</cite>
-                        </a>
-                    </li>
-                    <li>
-                        <a _href="echarts6.html">
-                            <i class="iconfont">&#xe6a7;</i>
-                            <cite>k线图</cite>
-                        </a>
-                    </li>
-                    <li>
-                        <a _href="echarts7.html">
-                            <i class="iconfont">&#xe6a7;</i>
-                            <cite>热力图</cite>
-                        </a>
-                    </li>
-                    <li>
-                        <a _href="echarts8.html">
-                            <i class="iconfont">&#xe6a7;</i>
-                            <cite>仪表图</cite>
-                        </a>
-                    </li>
-                </ul>
-            </li>
-=======
-            
->>>>>>> f62d8232ce501f2f471412a6157909e1b8b31274
->>>>>>> f28309157611839e0488bbbea5737a9b5aa7a4b6
+
         </ul>
       </div>
     </div>
